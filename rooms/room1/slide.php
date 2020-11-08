8055e941420d0fdaed408f44cb22c0dbc4460006
David Callizaya <davidcallizaya@gmail.com>
2020-11-08 10:51:46 -0400

    room 3

--- rooms/room1/slide.php
+++ rooms/room1/slide.php
@@ -1,4 +1,4 @@
-<h2 class="slide__name slideup"></h2>
+<h2 class="slide__name"></h2>
 <h3 class="slide__title">
     Somos una Organización sin fines de lucro conformada por voluntarios.
     <div class="slide__number">Nueva Acrópolis - La Paz</div>
--- rooms/room2/slide.php
+++ rooms/room2/slide.php
@@ -1,6 +1,5 @@
-<h2 class="slide__name slideup"></h2>
+<h2 class="slide__name"></h2>
 <h3 class="slide__title">
-    El arte y los procesos de transformación
-    <div class="slide__number">Nueva Acrópolis - La Paz</div>
+    Día mundial de la filosofía
 </h3>
-<p class="slide__date">11 de noviembre de 2020</p>
+<p class="slide__date">tercer jueves de noviembre de cada año</p>
