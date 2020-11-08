583ef6caba106352447bd25c9e7d617f39b6aa9e
David Callizaya <davidcallizaya@gmail.com>
2020-11-08 09:34:43 -0400

    Primer commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$leftTitle = '';
+$title = 'Galería Virtual';
+$description = 'Galería Viatual El Arte y los procesos de transformación';
+$keywords = '3d, room, exhibition, gallery, perspective, animation, web design, template';
+$author = 'Nueva Acropolis - La Paz';
+
---
+++ rooms/room1/slide.php
@@ -0,0 +1,6 @@
+<h2 class="slide__name slideup"></h2>
+<h3 class="slide__title">
+    El arte y los procesos de transformación
+    <div class="slide__number">Nueva Acrópolis - La Paz</div>
+</h3>
+<p class="slide__date">11 de noviembre de 2020</p>
